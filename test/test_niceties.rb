2ddb79b0b0e747d2d993145ac68494f026012eea
Carl Dawson <carldawson@hey.com>
2025-05-18 00:03:30 +0100

    Refactors tests by class

--- test/test_niceties.rb
+++ test/test_niceties.rb
@@ -3,10 +3,9 @@
 require "test_helper"
 require "ostruct"
 
-class NicetiesTest < Minitest::Test
+class ObjectNicetiesTest < Minitest::Test
   def test_not_a
     username = "carldaws"
-
     assert username.not_a? Integer
   end
 
@@ -24,23 +23,17 @@ class NicetiesTest < Minitest::Test
     user = OpenStruct.new(nickname: "", username: nil)
     assert_equal "carldaws", user.coalesce(:nickname, :username, "carldaws")
   end
+end
 
+class ArrayNicetiesTest < Minitest::Test
   def test_tidy
-    my_array = ["", nil, 4]
-
-    assert_equal [4], my_array.tidy
+    assert_equal [4], ["", nil, 4].tidy
   end
+end
 
-  def test_percent_of_integer
+class NumericNicetiesTest < Minitest::Test
+  def test_percent_of
     assert_equal 50, 25.percent_of(200)
-    assert_equal 0, 0.percent_of(200)
-    assert_equal 400, 200.percent_of(200)
-    assert_equal 0, 50.percent_of(0)
-  end
-
-  def test_percent_of_float
-    assert_equal 25.5, 50.0.percent_of(51)
     assert_equal 166, 16.6.percent_of(1000)
-    assert_equal 0, 123.4.percent_of(0)
   end
 end
